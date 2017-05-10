a505348e399e0006442f619a6086e9f25b2af5de
Kelvin <hpkelvin@163.com>
2017-05-10 22:51:01 +0800

    MarginWrapPanel is added.

---
+++ GUI/MarginWrapPanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ImageSearch
+{
+    class MarginWrapPanel: WrapPanel
+    {
+        public Thickness ItemMargin
+        {
+            get => itemMargin;
+            set
+            {
+                itemMargin = value;
+                foreach (UIElement u in Children)
+                {
+                    if (u is FrameworkElement f)
+                    {
+                        f.Margin = value;
+                    }
+                }
+            }
+        }
+        private Thickness itemMargin;
+    }
+}
